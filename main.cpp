ab84be01daf59ed8a876377da28ccb389e2194ca
brnd-from-mars <brendan.berg@icloud.com>
2022-03-07 23:27:43 +0100

    Add basic SFML window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <SFML/graphics.hpp>
 
 
 int main ()
 {
     std::cout << "Hello, World!" << std::endl;
+
+    sf::Window window(sf::VideoMode(800, 600), "Four Wins!");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+    }
+
     return 0;
 }
