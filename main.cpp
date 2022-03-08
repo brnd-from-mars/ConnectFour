aee03ed53846a5a9f56774ef479f4f7bfe58d185
brnd-from-mars <brendan.berg@icloud.com>
2022-03-08 21:07:53 +0100

    Add BaseView layers and make ViewContainer a list

--- main.cpp
+++ main.cpp
@@ -9,11 +9,12 @@ class ChipView : public BaseView
 
 public:
 
-    explicit ChipView(float centerX, sf::Color color)
+    explicit ChipView(float centerX, sf::Color color, int layer)
         : m_Circle(100)
     {
         m_Circle.setPosition(centerX-100, 200);
         m_Circle.setFillColor(color);
+        m_Layer = layer;
     }
 
     void Draw() override
@@ -34,8 +35,8 @@ int main()
 {
     AppDelegate::Get()->SetWindow(2560, 1440, "Four Wins!");
 
-    auto chip1 = std::make_shared<ChipView>(200.0f, sf::Color(84, 122, 165));
-    auto chip2 = std::make_shared<ChipView>(600.0f, sf::Color(204, 68, 75));
+    auto chip1 = std::make_shared<ChipView>(200.0f, sf::Color(84, 122, 165), 1);
+    auto chip2 = std::make_shared<ChipView>(300.0f, sf::Color(204, 68, 75), 2);
 
     AppDelegate::Get()->RegisterView(chip1);
     AppDelegate::Get()->RegisterView(chip2);
