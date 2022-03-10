bfba1a143def5b01e76cb0ecbdc39afbf366ab57
Fenrir-W <Florian-wolff@gmx.de>
2022-03-10 12:32:05 +0100

    Add Button, Add Parameters TextField

--- TextFieldController.cpp
+++ TextFieldController.cpp
@@ -26,7 +26,7 @@ TextFieldController::TextFieldController(float x, float y, float width)
     m_TextFieldModel = std::make_shared<TextFieldModel>();
     AppDelegate::Get()->RegisterModel(m_TextFieldModel);
 
-    m_TextFieldView = std::make_shared<TextFieldView>(); // x, y, width
+    m_TextFieldView = std::make_shared<TextFieldView>(x, y, width); // x, y, width
     AppDelegate::Get()->RegisterView(m_TextFieldView);
 }
 
