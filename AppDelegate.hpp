9c092d1686aec116e5dddff6586dc4cdfe263bdb
Fenrir-W <Florian-wolff@gmx.de>
2022-03-10 22:38:24 +0100

    Add Color Stuff, Change TextField

--- AppDelegate.hpp
+++ AppDelegate.hpp
@@ -21,12 +21,13 @@ namespace ColorPalette
     const sf::Color BasestarDark = sf::Color(8, 13, 21);
 
     const sf::Color Orange = sf::Color(233, 126, 22);
-    const sf::Color Cyan = sf::Color(111, 195, 223);
+    const sf::Color Cyan = sf::Color(0, 255, 255);
     const sf::Color Pane = sf::Color(230,255,255);
     const sf::Color SweetYellow = sf::Color(255,230,77);
     const sf::Color Gray = sf::Color(85,85,85);
     const sf::Color DarkBlue = sf::Color(6,36,116);
     const sf::Color PrussianBlue = sf::Color(0,49,83);
+    const sf::Color Pred = sf::Color(255, 0, 85);
 
 }
 
