c29f23a56c98c87a8cb8c6140323937d8165b179
brendanberg01 <brendan_berg@aol.com>
2022-03-10 21:40:11 +0100

    Outsource columns and rows into GameModel

---
+++ GameModel.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Brendan Berg on 10.03.22.
+//
+
+#include "GameModel.hpp"
+
+#include "GameController.hpp"
+
+
+GameModel::GameModel(int columns, int rows)
+    : m_Columns(columns), m_Rows(rows)
+{ }
+
+
+void GameModel::Update()
+{ }
