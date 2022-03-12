e220b849a0046888b950bedb1a851646bc922270
brnd-from-mars <brendan.berg@icloud.com>
2022-03-12 19:25:19 +0100

    Add layer argument to TextView

--- GameController.cpp
+++ GameController.cpp
@@ -26,8 +26,8 @@ GameController::GameController(int columns, int rows)
     m_GameModel = std::make_shared<GameModel>(columns, rows);
     AppDelegate::Get()->RegisterModel(m_GameModel);
 
-    m_HeadlineCyan = TextView::MakeText(48.0f, 22.0f, 75.0f, "Tron", "C0nnect F0ur", ColorPalette::Cyan);
-    m_HeadlineOrange = TextView::MakeText(52.0f, 26.0f, 75.0f, "Tron", "C0nnect F0ur", ColorPalette::Orange);
+    m_HeadlineCyan = TextView::MakeText(48.0f, 22.0f, 75.0f, "Tron", "C0nnect F0ur", ColorPalette::Cyan, 2);
+    m_HeadlineOrange = TextView::MakeText(52.0f, 26.0f, 75.0f, "Tron", "C0nnect F0ur", ColorPalette::Orange, 1);
 
     std::clog << "GameController constructed" << std::endl;
 }
