80385baa4ebfb6b625a90229a9e2bd4136e166c4
brnd-from-mars <brendan.berg@icloud.com>
2022-04-04 13:51:49 +0200

    Add comments to SessionController

--- SessionController.cpp
+++ SessionController.cpp
@@ -22,6 +22,7 @@ SessionController::MakeSessionController(const std::weak_ptr<GameController>& ga
     controller->m_SessionModel->m_SessionController = controller;
     controller->m_SessionController = controller;
 
+    // init UI and prepare for name entering
     controller->InitGrid();
     controller->InitGameTerminateButton();
     controller->InitNameTextFields();
@@ -39,6 +40,7 @@ SessionController::MakeSessionController(const SessionController& oldSessionCont
     controller->m_SessionModel->m_SessionController = controller;
     controller->m_SessionController = controller;
 
+    // init UI as if the names and colors were already picked in this round
     controller->InitGrid();
     controller->InitGameTerminateButton();
     controller->InitNameTextFields();
@@ -46,9 +48,11 @@ SessionController::MakeSessionController(const SessionController& oldSessionCont
     controller->InitClock();
     controller->InitGameRestartButton();
 
+    // copy entered names from previous session
     controller->m_NameTextFields[0]->SetText(oldSessionController.m_NameTextFields[0]->GetText());
     controller->m_NameTextFields[1]->SetText(oldSessionController.m_NameTextFields[1]->GetText());
 
+    // change colors if colors were changed in previous session
     if (controller->m_SessionModel->m_ColorsChanged)
     {
         controller->m_NameTextFields[0]->SetHighlightColor(ColorPalette::Cyan);
@@ -87,10 +91,14 @@ SessionController::~SessionController()
 
 void SessionController::Update()
 {
+    // position arrow next to text field of current player
     if (m_SessionModel->m_State == SessionState::inGame)
     {
+        // both arrow positions
         const sf::Vector2f arrowPositions[2] = {sf::Vector2f(1062.5f, 162.5f), sf::Vector2f(1162.5f, 212.5f)};
+        // get current player INDEX (0 for upper text field, 1 for lower, not to be confused by player NUMBER)
         auto position = arrowPositions[m_SessionModel->GetCurrentPlayerIndex()];
+        // set position of arrow
         m_CurrentPlayerArrow->SetPosition(position.x, position.y);
     }
 }
@@ -117,6 +125,7 @@ bool SessionController::IsTerminated() const
 
 void SessionController::InitGrid()
 {
+    // initiate all single grid fields in entire grid 2D vector
     m_Grid.resize(m_SessionModel->m_Columns);
     for (auto column = 0; column < m_SessionModel->m_Columns; ++column)
     {
@@ -148,8 +157,10 @@ void SessionController::InitGameTerminateButton()
 
 void SessionController::InitNameTextFields()
 {
+    // display "vs." text
     m_VSTextView = TextView::MakeText(765.0f, 185.0f, 40, "Tron", "VS.", ColorPalette::Orange);
 
+    // handler for text field enter press
     auto wController = m_SessionController;
     auto handler = [wController]()
                    {
@@ -159,6 +170,7 @@ void SessionController::InitNameTextFields()
                        }
                    };
 
+    // initiate both text fields with color, placeholder text and callback
     m_NameTextFields[0] = TextFieldController::MakeTextField(750.0f, 150.0f, 300.0f, "Player1", ColorPalette::Pred);
     m_NameTextFields[0]->RegisterEnterKeyPressCallback(handler);
 
@@ -169,14 +181,17 @@ void SessionController::InitNameTextFields()
 
 void SessionController::InitColorPickPrompt()
 {
+    // get random player name designated for color pick
     auto playerName = m_SessionModel->GetRandomPlayerForColorPick();
     playerName = playerName.append(":");
 
+    // show prompt for color pick
     m_StatusNameText = TextView::MakeText(750.0f, 250.0f, 20, "Standard", playerName, ColorPalette::Orange);
     m_StatusMessageText = TextView::MakeText(750.0f, 275.0f, 20, "Standard", "Pick your color!", ColorPalette::Orange);
 
     auto wController = m_SessionController;
 
+    // initiate both buttons with color, text and callback
     m_ColorPickButtons[0] = ButtonController::MakeButton(975.0f, 275.0f, 75.0f, "R3D", ColorPalette::Pred);
     m_ColorPickButtons[0]->RegisterMousePressCallback([wController]()
                                                       {
@@ -211,6 +226,7 @@ void SessionController::InitClock()
 
 void SessionController::InitGameRestartButton()
 {
+    // initiate restart button with color, text and callback
     m_GameRestartButton = ButtonController::MakeButton(750.0f, 625.0f, 120.0f, "R3START",
                                                          ColorPalette::Orange);
     auto wController = m_SessionController;
@@ -237,6 +253,7 @@ void SessionController::HandleNameEnter()
         return;
     }
 
+    // if names were entered for the first time validly -> init UI for color prompt
     InitColorPickPrompt();
 }
 
@@ -248,17 +265,20 @@ void SessionController::HandleColorPick(int color)
         return;
     }
 
+    // if colors were changed -> adapt UI
     if (m_SessionModel->m_ColorsChanged)
     {
         m_NameTextFields[0]->SetHighlightColor(ColorPalette::Cyan);
         m_NameTextFields[1]->SetHighlightColor(ColorPalette::Pred);
     }
 
+    // remove all UI elements for color pick
     m_StatusMessageText.reset();
     m_StatusNameText.reset();
     m_ColorPickButtons[0].reset();
     m_ColorPickButtons[1].reset();
 
+    // init UI for game round
     InitArrow();
     InitClock();
     InitGameRestartButton();
@@ -273,7 +293,10 @@ void SessionController::HandleGameRestartPress()
 
 void SessionController::HandleGameEnd(PlayerState winState, GameData game)
 {
+    // remove arrow
     m_CurrentPlayerArrow.reset();
+
+    // stop time and add it to results in GameData structure
     m_Clock->Stop();
     game.time = m_Clock->GetTotalSeconds();
 
@@ -283,9 +306,11 @@ void SessionController::HandleGameEnd(PlayerState winState, GameData game)
     }
     else if (winState == PlayerState::tie)
     {
+        // show tie-message
         m_StatusMessageText = TextView::MakeText(750.0f, 275.0f, 20, "Standard", "It's a tie!", ColorPalette::Orange);
 
-        game.winningPlayer = GetName(0); // TODO: outsource to
+        // add names to results in GameData structure (order of winningPlayer and loosingPlayer is negligible for tie)
+        game.winningPlayer = GetName(0); // TODO: outsource to SessionModel
         game.loosingPlayer = GetName(1);
 
         if (auto gameController = m_Game.lock())
@@ -295,11 +320,13 @@ void SessionController::HandleGameEnd(PlayerState winState, GameData game)
     }
     else
     {
+        // get winning player name and show victory-message
         auto playerName = game.winningPlayer;
         playerName = playerName.append(":");
         m_StatusNameText = TextView::MakeText(750.0f, 250.0f, 20, "Standard", playerName, ColorPalette::Orange);
         m_StatusMessageText = TextView::MakeText(750.0f, 275.0f, 20, "Standard", "You won!", ColorPalette::Orange);
 
+        // highlight 4 chips that lead to the victory
         for (auto chip : m_SessionModel->m_WinningChips)
         {
             m_Grid[chip.x][chip.y]->SetHighlightChip(true);
